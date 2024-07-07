99281b9208cbf71b9d8ed3df74700fd407bb6854
fullstack786 <fullstack786.dev01@gmail.com>
2024-07-07 19:18:30 +0500

    updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,15 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 async function dbConnect() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("Database connected");
-    } catch (err) {
-        console.error("Database connection error:", err);
-    }
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log("database connected"))
+        .catch((err) => console.log(err))
 }
 
-module.exports = dbConnect;
+module.exports = dbConnect
